9cbe29e5461fca496b8223ec8e43b2b9ca5dcd71
kobi krasnoff <krasnoff.kobi@gmail.com>
2021-11-16 21:57:07 +0200

    add icon font awesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './App.scss';
+import { faLaughWink } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <div id="wrapper">
       <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-          sdfsdfdsf
+            {/* <!-- Sidebar - Brand --> */}
+            <span className="sidebar-brand d-flex align-items-center justify-content-center">
+                <div className="sidebar-brand-icon rotate-n-15">
+                  <FontAwesomeIcon icon={faLaughWink} />
+                </div>
+                <div className="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+            </span>
       </ul>
     </div>
   );
