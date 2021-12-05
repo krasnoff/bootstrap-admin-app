ca279632069dc4e13a3821ca84b4d950b310544a
kobi krasnoff <krasnoff.kobi@gmail.com>
2021-12-05 21:43:45 +0200

    build main content window

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,20 @@ import Nav from './components/Navigator/navigator';
 function App() {
   return (
     <Router>
-    <div id="wrapper">
+    <div id="wrapper" className="wrapper">
       <Nav></Nav>
+      <div className="content-wrapper">
+        <div className="content">
+          <nav>fdgdfgdfgfdg</nav>
+        </div>
+        <footer className="sticky-footer">
+          <div className="container my-auto">
+              <div className="copyright text-center my-auto">
+                  <span>Copyright © Kobi Krasnoff Website 2021</span>
+              </div>
+          </div>
+        </footer>
+      </div>
     </div>
     </Router>
   );
