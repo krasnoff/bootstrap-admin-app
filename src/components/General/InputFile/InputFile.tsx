0f5b5da84d1a89240677627a115780c37f918243
kobi krasnoff <krasnoff.kobi@gmail.com>
2022-04-18 22:18:25 +0300

    continue design file upload

--- src/components/General/InputFile/InputFile.tsx
+++ src/components/General/InputFile/InputFile.tsx
@@ -12,15 +12,18 @@ function InputFile() {
     }
     
     return (
-        <div className="fileUpload">
-            <button className="fileUploadBtn" type="button">Add Image</button>
-            <div className="imageUploadWrap" ref={imageUploadWrap}>
-                <input className="fileUploadInput" type='file' accept="image/*" onChange={(input) => readURL(input)} />
+        <div className={styles.fileUpload}>
+            <button className={styles.fileUploadBtn} type="button">Add Image</button>
+            <div className={styles.imageUploadWrap} ref={imageUploadWrap}>
+                <input className={styles.fileUploadInput} type='file' accept="image/*" onChange={(input) => readURL(input)} />
+                <div className={styles.dragText}>
+                    <h3>Drag and drop a file or select add Image</h3>
+                </div>
             </div>
-            <div className="fileUploadContent" ref={fileUploadContent}>
-                <img className="fileUploadImage" src="#" alt="" ref={fileUploadImage} />
-                <div className="imageTitleWrap">
-                    <button type="button" className="removeImage">Remove <span className="imageTitle" ref={imageTitle}>Uploaded Image</span></button>
+            <div className={styles.fileUploadContent} ref={fileUploadContent}>
+                <img className={styles.fileUploadImage} src="#" alt="" ref={fileUploadImage} />
+                <div className={styles.imageTitleWrap}>
+                    <button type="button" className="removeImage">Remove <span className={styles.imageTitle} ref={imageTitle}>Uploaded Image</span></button>
                 </div>
             </div>
         </div>
