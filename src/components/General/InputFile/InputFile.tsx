0c87dd06d7fd7490fe5bd7ce2fed6e76bc599678
kobi krasnoff <krasnoff.kobi@gmail.com>
2022-04-20 22:39:21 +0300

    almost finish upload component

--- src/components/General/InputFile/InputFile.tsx
+++ src/components/General/InputFile/InputFile.tsx
@@ -6,16 +6,44 @@ function InputFile() {
     const fileUploadImage = useRef<HTMLImageElement>(null);
     const fileUploadContent = useRef<HTMLDivElement>(null);
     const imageTitle = useRef<HTMLSpanElement>(null);
+    // const fileUploadBtn = useRef<HTMLButtonElement>(null);
+    const fileUploadInput = useRef<HTMLInputElement>(null);
 
     const readURL = (input: any) => {
-        console.log('readURL', input);
+        const files = input.target.files;
+        if (files && files[0]) {
+            const reader = new FileReader();
+
+            reader.onload = function(e: any) {
+                imageUploadWrap.current?.style.setProperty('display', 'none');
+                fileUploadImage.current?.setAttribute('src', e.target.result);
+                fileUploadContent.current?.style.setProperty('display', 'block');
+
+                imageTitle.current!.innerHTML = files[0].name;
+                                
+            }
+
+            reader.readAsDataURL(files[0]);
+        } else {
+            removeUpload();
+        }
+    }
+
+    const openFilesBox = () => {
+        fileUploadInput.current?.click();
+    }
+
+    const removeUpload = () => {
+        fileUploadInput.current!.value = '';
+        fileUploadContent.current?.style.setProperty('display', 'none');
+        imageUploadWrap.current?.style.setProperty('display', 'block');
     }
     
     return (
         <div className={styles.fileUpload}>
-            <button className={styles.fileUploadBtn} type="button">Add Image</button>
+            <button className={styles.fileUploadBtn} type="button" onClick={() => openFilesBox()}>Add Image</button>
             <div className={styles.imageUploadWrap} ref={imageUploadWrap}>
-                <input className={styles.fileUploadInput} type='file' accept="image/*" onChange={(input) => readURL(input)} />
+                <input className={styles.fileUploadInput} type='file' accept="image/*" onChange={(input) => readURL(input)} ref={fileUploadInput} />
                 <div className={styles.dragText}>
                     <h3>Drag and drop a file or select add Image</h3>
                 </div>
@@ -23,7 +51,7 @@ function InputFile() {
             <div className={styles.fileUploadContent} ref={fileUploadContent}>
                 <img className={styles.fileUploadImage} src="#" alt="" ref={fileUploadImage} />
                 <div className={styles.imageTitleWrap}>
-                    <button type="button" className="removeImage">Remove <span className={styles.imageTitle} ref={imageTitle}>Uploaded Image</span></button>
+                    <button type="button" className={styles.removeImage}>Remove <span className={styles.imageTitle} ref={imageTitle} onClick={() => removeUpload()}>Uploaded Image</span></button>
                 </div>
             </div>
         </div>
