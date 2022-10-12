8c8887df4e62521689311e0cb3909b92e870194c
kobi krasnoff <krasnoff.kobi@gmail.com>
2022-10-12 23:02:24 +0300

    start simple form

---
+++ src/components/General/SimpleForm/simpleFile.tsx
@@ -0,0 +1,14 @@
+import styles from './InputFile.module.scss';
+
+function SimpleFile() {
+    
+    
+    return (
+        <div>
+            Simple Form
+        </div>
+    );
+  }
+  
+export default SimpleFile;
+  
--- src/pages/Form/Form.tsx
+++ src/pages/Form/Form.tsx
@@ -3,12 +3,16 @@ import stylesText from '../../css/texts.module.scss';
 import stylesMargins from '../../css/margins.module.scss';
 import ComponentWrapper from '../../components/General/ComponentWrapper/ComponentWrapper';
 import InputForm from '../../components/General/InputFile/InputFile';
+import SimpleForm from '../../components/General/SimpleForm/simpleFile';
 
 
 function FormComponent() {
     return (
         <div className={styles.containerFluid}>
             <h1 className={[stylesText.h3, stylesMargins.mb0, stylesText.textGray800].join(' ')}>Form Component</h1>
+            <ComponentWrapper title='Simple Form'>
+                <div><SimpleForm></SimpleForm></div>
+            </ComponentWrapper>
             <ComponentWrapper title='Market Summary'>
                 <div><InputForm></InputForm></div>
             </ComponentWrapper>
