6fd8060b8d76575118c3d529f0f6dc843cb771a7
kobi krasnoff <krasnoff.kobi@gmail.com>
2023-03-01 23:07:28 +0200

    useCallback try to figure out

--- src/pages/SubMenu3/useCalback/sampleUseContext.tsx
+++ src/pages/SubMenu3/useCalback/sampleUseContext.tsx
@@ -1,13 +1,23 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Data } from "./data";
 
 export default function SampleUseCallback() {
   const [data, setData] = useState<Data>()
+  const [isAlive, seIsAlive] = useState<boolean>()
   
-  const handleSubmit = useCallback(async () => {
+  const handleSubmit = /*useCallback(*/async () => {
     const data = await fetch('https://reqres.in/api/users?page=2', {});
-    setData(data as unknown as Data);
-  }, [data]);
+    const parsedData = await data.json();
+    setData(parsedData as unknown as Data);
+  }/*, [data]);*/
+
+  const isAliveHandle = () => {
+    seIsAlive(!isAlive)
+  }
+
+  useEffect(() => {
+    handleSubmit();
+  }, []);
 
   // TODO call REST on loading by useEffect 
   // set another state see rendering.
@@ -16,6 +26,9 @@ export default function SampleUseCallback() {
     <div>
       useCallback<br />
       <button onClick={handleSubmit}>submit</button>
+      <button onClick={isAliveHandle}>{isAlive ? 'is Alive' : 'not alive'}</button>
+      <div>{(new Date()).toString()}</div>
+      <div>{JSON.stringify(data)}</div>
     </div>    
   );
 }
